e3949d8d1e6fa22e6fcdaa435852bcd359b7e946
LeeJoonBin <dlwnsqls22@gmail.com>
2026-09-08 11:46:51 +0900

    ㅇㅇ

--- 26.09.07_Class/Assets/Scripts/FlameEffect.cs
+++ 26.09.07_Class/Assets/Scripts/FlameEffect.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class FlameEffect : MonoBehaviour
 {
+    [SerailizeField] private float _deactivateDelay;
+    private float _elapsedTime;
     
+    private void OnEnable() => ResetElapsedTime();
+    private void Start() => gameObject.SetActive(false);
+
+    private void Update()
+    {
+        UpdatyeElapsedTime();
+        Deactivate();
+
+    }
+
+
 }
